34da73b543d44b5d0f724dd35c34a7ba5cce2078
ayanakkabi1 <aynour200609@gmail.com>
2026-03-16 11:48:01 +0000

    LAC-17 feat: implement Product model with fillable attributes and relationships to Category, ProductImage, and OrderItem

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
