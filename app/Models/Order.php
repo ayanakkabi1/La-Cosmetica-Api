f5bae51d4958e66531860695d18e97648638b3af
ayanakkabi1 <aynour200609@gmail.com>
2026-03-16 11:47:03 +0000

    LAC-9 feat: implement Order model with fillable attributes and relationships to User and OrderItem

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable=[
+        'user_id' ,
+        'status' ,
+        'total_price'
+    ];
+    public function user(){
+        return $this->belongTo(User::class);
+    }
+    public function items(){
+        return $this->hasmany(OrderItem::class);
+    }
 }
