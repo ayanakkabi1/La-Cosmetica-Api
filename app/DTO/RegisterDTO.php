6c71e711c33c5757b47a7b84af4a7c58776c85cf
ayanakkabi1 <aynour200609@gmail.com>
2026-03-17 12:35:02 +0000

    LAC-3 feat: create RegisterDTO for user registration data handling

---
+++ app/DTO/RegisterDTO.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\DTO;
+class RegisterDTO
+{
+    public string $name;
+    public string $email;
+    public string $password;
+
+    public function __construct(array $data)
+    {
+        $this->name = $data['name'];
+        $this->email = $data['email'];
+        $this->password = $data['password'];
+    }
+}
