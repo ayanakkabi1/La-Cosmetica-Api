5dd4311639d8fd9b90a80ee6754db5fb12043de4
ayanakkabi1 <aynour200609@gmail.com>
2026-03-17 12:37:40 +0000

    LAC-5 feat: implement JWTSubject interface in User model for JWT authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -50,5 +51,14 @@ class User extends Authenticatable
      public function orders()
     {
         return $this->hasMany(Order::class);
+    }
+     public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
