6d47477bb49df6e93c987f95bab1a13808e98405
ayanakkabi1 <aynour200609@gmail.com>
2026-03-18 11:05:30 +0000

    LAC 10/11/12/ 13 feat: implement UserDAO for user data access operations

---
+++ app/DAO/UserDAO.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\DAO;
+use App\Models\User;
+class UserDAO
+{
+    public function create(array $data)
+    {
+        return User::create($data);
+    }
+
+    public function findByEmail(string $email)
+    {
+        return User::where('email', $email)->first();
+    }
+}
